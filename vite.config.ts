f9f42d982320664682ba4ba39b0ea9a294464d1e
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-09-26 13:52:06 +0300

    fix: Vite base for GH page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/api-docs/", // Set the base path for GitHub Pages
   plugins: [react()],
   build: {
     rollupOptions: {
